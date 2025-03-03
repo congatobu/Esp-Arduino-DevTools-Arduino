eeeea7b8807bd68ca4c50e696c4beda02b426286
congatobu <hydra2k5@gmail.com>
2025-03-03 15:07:20 +0700

    Log-ESP32

--- Log-ESP32/src/main.cpp
+++ Log-ESP32/src/main.cpp
@@ -1,18 +1,49 @@
 #include <Arduino.h>
+#include <BluetoothSerial.h>
 
-// put function declarations here:
-int myFunction(int, int);
+BluetoothSerial bluetoothSerial;
 
-void setup() {
+void TaskSendData(void *pvParameters)
+{
+  while (true)
+  {
+    Serial.println("gui du lieu len server");
+    bluetoothSerial.println("{hello: 1}");
+    vTaskDelay(5000 / portTICK_PERIOD_MS);
+  }
+}
+
+void setup()
+{
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  bluetoothSerial.begin("ESP32-BT");
+  Serial.println("Bluetooth SPP start...");
+  delay(5000);
+
+ 
+  
+
+  xTaskCreatePinnedToCore(TaskSendData, "Send Data ", 2000, NULL, 1, NULL, 1);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void loop()
+{
+  if (bluetoothSerial.available() > 0)
+  {
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+    String data = bluetoothSerial.readStringUntil('\n');
+    Serial.println(data);
+  }
+
+  if (bluetoothSerial.available()> 0)
+  {
+    char incomingChar = bluetoothSerial.read();
+    Serial.write(incomingChar); // In dữ liệu nhận được ra Serial Monitor
+  }
+
+  if (Serial.available())
+  {
+    bluetoothSerial.write(Serial.read());
+  }
 }
